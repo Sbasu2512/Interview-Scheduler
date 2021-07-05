a7cc6a415555a76ddaba68f55b7a87197de83cf4
Sayantan Basu <sayantanbasu@live.in>
2021-07-05 17:51:25 -0400

    finish refactor btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,26 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classname = require('classnames');
+
+
 export default function Button(props) {
    console.log("---------PROPS-----------", props);
 
-   let buttonClass = "button" ;
+   let buttonClass = classname("button", {
+    "button--confirm": props.confirm,
+    "button--danger":props.danger
+   })
 
-   if(props.confirm){
-      buttonClass += "button--confirm";
-   }
+   //  let buttonClass = "button";
 
-   if(props.danger){
-      buttonClass += "button--danger";
-   }
+  //  if(props.confirm){
+  //     buttonClass += "button--confirm";
+  //  }
+
+  //  if(props.danger){
+  //     buttonClass += "button--danger";
+  //  }
    
    return (
      <
