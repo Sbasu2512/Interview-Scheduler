28fb586d0be40e2ffeb7a60161365e678d695828
Sayantan Basu <sayantanbasu@live.in>
2021-07-07 01:26:45 -0400

    complete appointment views

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,8 @@ import DayList from 'components/daylist';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
-import Header from "components/Appointment/header"
+import Header from "components/Appointment/header";
+import Empty from "components/Appointment/Empty";
 
 //styles
 import "index.scss";
@@ -145,4 +146,5 @@ storiesOf("Button", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment
   time = "12.00" />)
-  .add("Header", () => <Header time = "12pm" /> );
+  .add("Header", () => <Header time = "12pm" /> )
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
