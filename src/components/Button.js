57ca16d8abf4eb416752cc53cffc6071eda21cf3
Sayantan Basu <sayantanbasu@live.in>
2021-07-15 19:41:48 +0000

    linting-btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,13 @@
 import React from "react";
-
 import "./Button.scss";
-
 const classname = require('classnames');
 
-
 export default function Button(props) {
-  //  console.log("---------PROPS-----------", props);
 
    let buttonClass = classname("button", {
     "button--confirm": props.confirm,
     "button--danger":props.danger
    })
-
-   //  let buttonClass = "button";
-
-  //  if(props.confirm){
-  //     buttonClass += "button--confirm";
-  //  }
-
-  //  if(props.danger){
-  //     buttonClass += "button--danger";
-  //  }
    
    return (
      <
